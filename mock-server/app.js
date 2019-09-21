dfcb2eda6b1d318db88fbe55ca5589c6cc798d88
DESKTOP-NGQC278\jack <330447171@qq.com>
2019-09-21 21:24:18 +0800

    mock代理

--- mock-server/app.js
+++ mock-server/app.js
@@ -1,9 +1,30 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const fs = require('fs');
+const proxyMiddleWare= require('http-proxy-middleware');
 
+const proxyOption1 ={
+  target: 'http://localhost:3000',
+  changeOrigoin: true,
+  ws: true,
+  pathRewrite: { '^/test1': '/' }
+};
+
+const proxyOption2 ={
+  target: 'http://localhost:3000',
+  changeOrigoin: true,
+  ws: true,
+  pathRewrite: { '^/test2': '/' }
+};
+
+// 指定根目录
 app.use(express.static(__dirname + "/" , {index:"index.html"}));
 
-app.use(express.static('dist'))
+// 代理
+// app.use('/test1', express.static('./index.html'));
+// app.use('/test2', express.static('./index.html'));
+app.use("/test1", proxyMiddleWare(proxyOption1));
+app.use("/test2", proxyMiddleWare(proxyOption2));
 
+// 哒哒哒
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
